ac0bae553ad0ebdb4ceffd0babbbcbc09ad5286a
skc <sheikouz@gmail.com>
2026-01-12 22:38:27 +0100

    [fix/ex00] - Fix some bugs

--- ex00/test.sh
+++ ex00/test.sh
@@ -2,8 +2,6 @@
 
 PROGRAM="./convert"
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
@@ -22,27 +20,9 @@ if [ ! -x "$PROGRAM" ]; then
 fi
 
 print_section() {
-    echo ""
-    echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+    echo -e "\n${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
     echo -e "${BLUE}$1${NC}"
     echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-}
-
-test_output() {
-    local test_name="$1"
-    local input="$2"
-
-    TOTAL=$((TOTAL + 1))
-
-
-    if [ "$input" == "NO_ARG" ]; then
-        actual=$($PROGRAM 2>&1)
-    else
-        actual=$($PROGRAM "$input" 2>&1)
-    fi
-    echo -e "${RED}✗${NC} Test $TOTAL: $test_name"
-    echo "  Input: '$input'"
-    echo "Got: $actual" | sed 's/^/    /'
 }
 
 test_contains() {
@@ -53,12 +33,10 @@ test_contains() {
 
     actual=$($PROGRAM "$input" 2>&1)
  
-    echo -e "[$YELLOW Test $TOTAL: $test_name $NC]"
-    echo ""
-	echo "input: '$input'"
-    echo ""
-    echo "$actual" | sed 's/^/    /'
-	echo "--------------------------------------------------------"
+    echo -e "[$YELLOW Test $TOTAL: $test_name $NC]\n"
+	echo -e "input: '$input'\n"
+    echo -e "$actual\n"
+	echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 }
 
 print_section "Tests Chars"
@@ -81,17 +59,19 @@ test_contains "Float 42.0f" "42.0f"
 test_contains "Float 42.42f" "42.42f"
 test_contains "Float negativ" "-42.42f"
 test_contains "Float 0.0f" "0.0f"
+test_contains "Float big nbr" "999999999999999.7f"
 
 print_section "Tests Doubles"
 
 test_contains "Double 42.0" "42.0"
 test_contains "Double 42.42" "42.42"
 test_contains "Double negativ" "-42.42"
+test_contains "double 0.0" "0.0"
+test_contains "double big nbr" "999999999999999.7"
 
 print_section "Tests Non_numbers"
 
 test_contains "nan" "nan"
-test_contains "nan - float" "nan"
 test_contains "nanf" "nanf"
 test_contains "+inf" "+inf"
 test_contains "-inf" "-inf"
@@ -101,11 +81,12 @@ test_contains "-inff" "-inff"
 print_section "Tests Edge cases"
 
 test_contains "Overflow int" "2147483648"
-test_contains "Underflow int" "-2147483649"
-test_contains "Char hors limites (pos)" "200"
-test_contains "Char hors limites (neg)" "-200"
-test_contains "String vide" ""
-test_contains "Point seul" "."
+test_contains "Overflow float" "350000000000000000000000000000000000000.f"
+test_contains "Overflow float from double conversion" "350000000000000000000000000000000000000."
+test_contains "Char out of bound (pos)" "200"
+test_contains "Char out of boynd (neg)" "-200"
+test_contains "Empty string" ""
+test_contains "Dot only" "."
 
 print_section "Tests Invalid inputs"
 
@@ -116,6 +97,4 @@ test_contains "Double f" "42.0ff"
 test_contains "Minus dot " "-."
 test_contains "Space" "42.0f 1"
 
-echo ""
-echo "Total:  $TOTAL tests"
 echo ""
